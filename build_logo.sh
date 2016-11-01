d0197f5f8fbddc5e96cb134899d1a972fa7ec349
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-11-01 22:16:47 +0000

    Modify build_logo task and functional test to output to PBM format

--- build_logo.sh
+++ build_logo.sh
@@ -2,7 +2,7 @@
 # 
 # Script for updating the project logo
 # The first argument is the path to the sxp cli program.
-# The second argument is the file to write the PNG output to.
+# The second argument is the file to write the PBM output to.
 # The third is the message to use for the logo data
 # (of format "saxbospiral vX.Y.Z").
 # 
--- func_test.sh
+++ func_test.sh
@@ -5,6 +5,6 @@
 # The first argument is the path to the sxp cli program.
 # The second argument is the message to use for the file (see build_logo.sh)
 # 
-./build_logo.sh "$1" "test_sxbp.png" "$2" && \
-diff "sxbp.png" "test_sxbp.png" && \
-rm "test_sxbp.png";
+./build_logo.sh "$1" "test_sxbp.pbm" "$2" && \
+diff "sxbp.pbm" "test_sxbp.pbm" && \
+rm "test_sxbp.pbm";
